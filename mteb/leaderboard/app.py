029beb79043aea441b825387c072ee69180c8622
q275343119 <275343119@qq.com>
2025-11-26 15:42:38 +0800

    feat - do not allow_custom_value

--- mteb/leaderboard/app.py
+++ mteb/leaderboard/app.py
@@ -247,7 +247,7 @@ def get_leaderboard_app() -> gr.Blocks:
     task_select = gr.Dropdown(
         sorted(all_results.task_names),
         value=sorted(default_results.task_names),
-        allow_custom_value=True,
+        allow_custom_value=False,
         multiselect=True,
         label="Task",
         info="Select specific tasks to include",
